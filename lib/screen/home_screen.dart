584bf791efacaae8fb5da5283d55ef22351277fe
용석 박 <fabbro@infograb.net>
2024-01-08 23:13:07 +0900

    콘텐츠카드 리스트로 보여주기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mfp/component/ContentCard.dart';
+
+const DUMY_LIST = ['KT', '카카오톡', '네이버'];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text('하이'),
-    ));
+    return SafeArea(
+      child: Scaffold(
+          body: ListView.separated(
+        itemCount: 3,
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 8.0);
+        },
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ContentCard(title: DUMY_LIST[0]),
+          );
+        },
+      )),
+    );
   }
 }
